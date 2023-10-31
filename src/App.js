9c64f9ae6d5da9c6aec4a6389bfaafe271a84f81
RaviD1904 <ravid1904@gmail.com>
2023-10-31 17:09:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { Layout } from "antd";
+import "./App.css";
+import "./index.css";
+import Dashboard from "./pages/dashboard";
+import { useState } from "react";
+import Login from "./pages/login";
+const { Header } = Layout;
 
 function App() {
+  const [login, setLogin] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {!login ? (
+        <div className="flex justify-center" style={{
+          width:"100%",
+          height:"100%",
+          // border:"1px solid red",
+          marginTop:"10%"
+        }}>
+
+          <Login  setLogin={setLogin}/>
+        </div>
+      ) : (
+        <div>
+          <Header
+            style={{
+              position: "sticky",
+              top: 0,
+              zIndex: 1,
+              width: "100%",
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <div
+              className="text-white font-serif animate-marquee whitespace-nowra font-bold text-3xl"
+              style={{
+                width: "100%",
+                color: "white",
+              }}
+            >
+              Shreerang Ayurved Clinic And Panchkarm Center
+            </div>
+          </Header>
+          <Dashboard />
+        </div>
+      )}
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  mode: 'jit',
+  theme: {
+    extend: {
+      animation: {
+        marquee: 'marquee 10s linear infinite',
+        marquee2: 'marquee2 5s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(100%)' },
+        },
+        marquee2: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
+      },
+    },
+  },
+  plugins: [],
+}
